5779d922284a0d1cd99c092f473dfaef80e30d75
Jaskaran singh <JaskaranSingh2@edu.sait.ca>
2025-01-27 00:48:34 -0700

    added title component

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import Title from "./student-info.js";
+
+export default function Page() {
+    return (
+        <main>
+            <Title />
+            <h1>Shopping List</h1>
+        </main>
+    );
+}
