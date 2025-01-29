61f8265168cbc052823666251ab7bab866068970
Jaskaran singh <JaskaranSingh2@edu.sait.ca>
2025-01-29 15:37:47 -0700

    Week-4

--- app/page.js
+++ app/page.js
@@ -6,7 +6,9 @@ export default function Page() {
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
-        <Link href="week-2">Week 2</Link>
+        <Link href="week-2">Week 2</Link><br></br>
+        <Link href="week-3">Week 3</Link><br></br>
+        <Link href="week-4">Week 4</Link><br></br>
       </p>
     </div>
   );
